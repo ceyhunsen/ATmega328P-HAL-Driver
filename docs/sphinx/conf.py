d6a6684f5423e03ff870e22e0a43401183570865
Ceyhun Şen <ceyhunsen237@gmail.com>
2022-12-28 23:29:47 +0300

    Fix for RTD.

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -5,6 +5,8 @@ import subprocess
 project = "ATmega328P HAL Driver"
 copyright = "2022, Ceyhun Şen"
 author = "Ceyhun Şen"
-html_extra_path = ["build/html"]
 
-subprocess.call("doxygen Doxyfile", shell = True)
+html_extra_path = ["../build/html"]
+html_path = ["../build/html"]
+
+subprocess.call("cd ..; doxygen Doxyfile", shell = True)
